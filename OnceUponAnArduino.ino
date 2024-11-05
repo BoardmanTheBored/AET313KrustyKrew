69c5a2c94ca0a1f4d63bb45e039c172168e2600e
BoardmanTheBored <anthonyboardman000@gmail.com>
2024-11-05 13:21:26 -0600

    Add files via upload

---
+++ OnceUponAnArduino.ino
@@ -0,0 +1,40 @@
+#include <Servo.h>
+
+const int SignSwitchPin = 1;
+const int SquidwardSwitchPin = 2;
+const int SpongebobSwitchPin = 3;
+const int SpongebobSpatulaPin = 4;
+const int PhoneSwitchPin = 5;
+const int SlasherSwitchPin = 6;
+const int SlasherSpatulaPin = 7;
+
+const int LightsLEDPin = 8;
+const int PhoneLEDPin = 9;
+
+Servo ChestLid = 10;
+Servo SpongebobDoor = 11;
+Servo Bus = 12;
+Servo SlasherTrack = 13;
+Servo Front Doors = 14;
+Servo Curtains = 15;
+
+void setup() {
+  PinMode(SignSwitchPin, INPUT);
+  PinMode(SquidwardSwitchPin, INPUT);
+  PinMode(SpongebobSwitchPin, INPUT);
+  PinMode(SpongebobSpatulaPin, INPUT);
+  PinMode(PhoneSwitchPin, INPUT);
+  PinMode(SlasherSwitchPin, INPUT);
+  PinMode(SlasherSpatulaPin, INPUT);
+  PinMode(LightsLEDPin, OUTPUT);
+  PinMode(PhoneLEDPin, OUTPUT);
+
+
+
+  
+}
+
+void loop() {
+  
+
+}
